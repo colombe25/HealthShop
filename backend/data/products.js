e2bc72b0b79d7e5a8b8fa6c825391f5548b2f266
oumou kouyate <colombe.1@hotmail.com>
2020-12-24 15:16:40 +0100

    projet

---
+++ backend/data/products.js
@@ -0,0 +1,33 @@
+const products = [
+  {
+    _id: "1",
+    name: "Fruit box",
+    images: "/images/Fruit.jpg",
+    description:
+      "Fuit of the season grown with the respect of the envrironment",
+    category: "Fruit",
+    Instock: 3,
+    rating: 4,
+  },
+  {
+    _id: "2",
+    name: "Veggie box",
+    images: "images/vegetable.png",
+    description:
+      "veggie of the season grown with the respect of the envrironment",
+    category: "  Veggie ",
+    Instock: 3,
+    rating: 4,
+  },
+  {
+    _id: "3",
+    name: "Mixt box",
+    images: "images/FruitVeggie.jpg",
+    description:
+      "Fuit of the season grown with the respect of the envrironment",
+    category: "Mix ",
+    Instock: 3,
+    rating: 4,
+  },
+];
+export default products;
